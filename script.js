0bde7d93fcb116c3aaf4bf89a5dd47bf91e9826b
Tolgahan <celiktolgaa08@gmail.com>
2022-06-02 20:42:58 +0300

    ModalWindow

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+
+for (let i=0;i<btnsOpenModal.length; i++)
+btnsOpenModal[i].addEventListener('click',function(){
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden')
+})
+const closemodal=function(){
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+
+}
+btnCloseModal.addEventListener('click',function(){
+ closemodal()
+})
+document.addEventListener('keydown',function(e){
+  console.log(e.key)
+  if(e.key==='Escape'&& !modal.classList.contains('hidden')){
+    closemodal()
+  }
+})  
